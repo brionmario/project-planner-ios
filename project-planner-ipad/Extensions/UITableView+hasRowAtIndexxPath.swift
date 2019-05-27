ed69f5b2afeea911e30b3adb3c1a98ed7d693c61
Brion Mario <brion.2015283@iit.ac.lk>
2019-05-27 09:53:16 +0530

    refactor(ext): remove unwanted extension

--- project-planner-ipad/Extensions/UITableView+hasRowAtIndexxPath.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UITableView+hasRowAtIndexxPath.swift
-//  project-planner-ipad
-//
-//  Created by Brion Silva on 25/05/2019.
-//  Copyright © 2019 Brion Silva. All rights reserved.
-//
-
-import Foundation
